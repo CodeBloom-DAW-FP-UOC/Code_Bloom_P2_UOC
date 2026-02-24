76a76bc1e4d48d837ee4bbd7b753ed973261e193
ChampCTE <celia.trulla@gmail.com>
2026-02-24 18:40:40 +0100

    Proyecto Base AlquilaTusVehiculos

---
+++ src/main/java/CodeBloom/AlquilaTusVehiculos/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package CodeBloom.AlquilaTusVehiculos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping ("/")
+public class HelloWorld {
+
+    @GetMapping ("/hello")
+    public String hello(){
+        return "Hola Mundo! Somos Code Bloom! Venimos a darlo todo!";
+    }
+}
