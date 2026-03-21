301ae1c558bcba9597e743e9ab42f60970b89ce9
milena <darimile@gmail.com>
2026-03-21 11:58:40 +0100

    Feature: implementación del modelo Rental y relación con User

---
+++ src/main/java/CodeBloom/AlquilaTusVehiculos/models/Rental.java
@@ -0,0 +1,34 @@
+package CodeBloom.AlquilaTusVehiculos.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "rentals")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Rental {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime startDate;
+    private LocalDateTime estimatedReturnDate;
+    private LocalDateTime returnDate;
+
+    private Double price;
+    private String note;
+    private String state;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    //@ManyToOne
+    //@JoinColumn(name = "vehicle_id")
+    //private Vehicle vehicle;
+}
